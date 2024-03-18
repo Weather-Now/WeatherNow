cdae2a289c0893a968eac2eee69213829b7e7b83
angelicacc00 <angelicac0721@gmail.com>
2024-03-18 14:35:30 -0400

    Form Component

---
+++ weather-now/src/components/Form.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+const Form = () => {
+  const [formData, setFormData] = useState({city: ''})
+
+  const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormData({...formData, [name]: value})
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('Form Submitted:', formData)
+  }
+return (
+  <div>
+    <form onSubmit={handleSubmit}>
+      <input
+      type="text"
+      name="city"
+      value={formData.city}
+      onChange={handleChange}
+      placeholder="Enter City"
+      />
+      <button type="submit">Submit</button>
+   </form>
+  </div>
+  )
+}
+export default Form;
