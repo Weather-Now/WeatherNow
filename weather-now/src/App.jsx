aebbd0d050c1b0ebb78e01b10dd5f76dbd946d48
Rafi Barides <145809035+Rafibarides@users.noreply.github.com>
2024-03-18 15:09:56 -0400

    LocalWeather componenet done

--- weather-now/src/App.jsx
+++ weather-now/src/App.jsx
@@ -1,13 +1,20 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import LocalWeather from './components/LocalWeather';
+import LocalSunrise from './components/LocalSunrise'; 
 
 function App() {
-
-
   return (
-    <>
-    </>
-  )
+    <main>
+    <Router>
+      <Routes>
+        <Route path="/" element={<LocalWeather />} />
+        <Route path="/LocalSunrise" element={<LocalSunrise />} />
+      </Routes>
+    </Router>
+    </main>
+  );
 }
 
-export default App
+export default App;
---
+++ weather-now/src/utils/weatherSymbols.js
@@ -0,0 +1,10 @@
+const weatherSymbols = [
+    "https://i.ibb.co/TrKbLmF/cloudy.png",
+    "https://i.ibb.co/YjFcDg9/Partly-cloudy.png",
+    "https://i.ibb.co/2qB2K7j/Rain.png",
+    "https://i.ibb.co/x5kKJbg/Snow.png",
+    "https://i.ibb.co/9ZMDYSR/Sunny.png",
+    "https://i.ibb.co/FKjFmTG/thunderstorm.png",
+    "https://i.ibb.co/BV5fRk1/windy.png"
+  ];
+  
